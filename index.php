585b6106969a3843be1f9f2650a526bc1cde89e3
Arshad Chummun <arshad@chummun.com>
2013-08-01 23:03:09 +0400

    Removed yeoman for now

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="assets/stylesheets/screen.css">
+    <script src="scripts/vendor/modernizr.min.js"></script>
+  </head>
+  <body>
+  <div class="navbar navbar-inverse">
+    <div class="navbar-inner">
+      <div class="container">
+        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="brand" href="#">White</a>
+        <div class="nav-collapse">
+          <ul class="nav pull-right">
+            <li class="active active-trail"><a href="#" class="active">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <!-- Read about Bootstrap dropdowns at http://twbs.github.com/bootstrap/javascript.html#dropdowns -->
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="nav-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-inner -->
+  </div><!-- /.navbar -->
+
+  <header role="header" id="header">
+    <div class="container">
+    </div>
+  </header>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="scripts/vendor/jquery.min.js"><\/script>')</script>
+  
+  <!-- build:js scripts/plugins.js -->
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-affix.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-alert.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-dropdown.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-tooltip.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-modal.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-transition.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-button.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-popover.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-typeahead.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-carousel.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-scrollspy.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-collapse.js"></script>
+  <script src="assets/javascripts/vendor/bootstrap/bootstrap-tab.js"></script>
+  <!-- endbuild -->
+</body>
+</html>
